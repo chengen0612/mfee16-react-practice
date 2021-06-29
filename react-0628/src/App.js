ecc909c5ed9d02620317294aec0f6b08ccf55565
chengen0612 <chengen0612@gmail.com>
2021-06-30 00:16:36 +0800

    update: finish simple cart

--- react-0628/src/App.js
+++ react-0628/src/App.js
@@ -1,132 +1,62 @@
 import React, { useState } from 'react'
 // import css
 import './App.css'
+import Summary from './Components/Summary'
+import OrderList from './Components/OrderList'
+
+const products = [
+  {
+    name: '咖啡色 T-shirt',
+    catgory: 'Shirt',
+    image: 'https://i.imgur.com/1GrakTl.jpg',
+    price: 300,
+  },
+  {
+    name: '白色 T-shirt',
+    catgory: 'Shirt',
+    image: 'https://i.imgur.com/ba3tvGm.jpg',
+    price: 200,
+  },
+  {
+    name: '黑色 T-shirt',
+    catgory: 'Shirt',
+    image: 'https://i.imgur.com/pHQ3xT3.jpg',
+    price: 450,
+  },
+  {
+    name: '金色 T-shirt',
+    catgory: 'Shirt',
+    image: 'https://i.imgur.com/pHQ3xT3.jpg',
+    price: 1200,
+  },
+]
 
 function App() {
+  // set default number
+  const [counts, setCounts] = useState(Array(products.length).fill(1))
+
+  const sumUp = counts.reduce(
+    (accumulator, currentValue) => accumulator + currentValue
+  )
+
+  const total = () => {
+    let total = 0
+    products.forEach((product, i) => {
+      total += product.price * counts[i]
+    })
+    return total
+  }
+
   return (
     <>
       <div className="card">
         <div className="row">
-          <div className="col-md-8 cart">
-            <div className="title">
-              <div className="row">
-                <div className="col">
-                  <h4>
-                    <b>Shopping Cart</b>
-                  </h4>
-                </div>
-                <div className="col align-self-center text-right text-muted">
-                  3 items
-                </div>
-              </div>
-            </div>
-            <div className="row border-top border-bottom">
-              <div className="row main align-items-center">
-                <div className="col-2">
-                  <img
-                    className="img-fluid"
-                    src="https://i.imgur.com/1GrakTl.jpg"
-                    alt=""
-                  />
-                </div>
-                <div className="col">
-                  <div className="row text-muted">Shirt</div>
-                  <div className="row">Cotton T-shirt</div>
-                </div>
-                <div className="col">
-                  <a href="#/">-</a>
-                  <a href="#/" className="border">
-                    1
-                  </a>
-                  <a href="#/">+</a>
-                </div>
-                <div className="col">
-                  &euro; 44.00 <span className="close">&#/10005;</span>
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="row main align-items-center">
-                <div className="col-2">
-                  <img
-                    className="img-fluid"
-                    src="https://i.imgur.com/ba3tvGm.jpg"
-                    alt=""
-                  />
-                </div>
-                <div className="col">
-                  <div className="row text-muted">Shirt</div>
-                  <div className="row">Cotton T-shirt</div>
-                </div>
-                <div className="col">
-                  <a href="#/">-</a>
-                  <a href="#/" className="border">
-                    1
-                  </a>
-                  <a href="#/">+</a>
-                </div>
-                <div className="col">
-                  &euro; 44.00 <span className="close">&#/10005;</span>
-                </div>
-              </div>
-            </div>
-            <div className="row border-top border-bottom">
-              <div className="row main align-items-center">
-                <div className="col-2">
-                  <img
-                    className="img-fluid"
-                    src="https://i.imgur.com/pHQ3xT3.jpg"
-                    alt=""
-                  />
-                </div>
-                <div className="col">
-                  <div className="row text-muted">Shirt</div>
-                  <div className="row">Cotton T-shirt</div>
-                </div>
-                <div className="col">
-                  <a href="#/">-</a>
-                  <a href="#/" className="border">
-                    1
-                  </a>
-                  <a href="#/">+</a>
-                </div>
-                <div className="col">
-                  &euro; 44.00 <span className="close">&#/10005;</span>
-                </div>
-              </div>
-            </div>
-            <div className="back-to-shop">
-              <a href="#/">←</a>
-              <span className="text-muted">Back to shop</span>
-            </div>
-          </div>
-          <div className="col-md-4 summary">
-            <div>
-              <h5>
-                <b>Summary</b>
-              </h5>
-            </div>
-            <hr />
-            <div className="row">
-              <div className="col order__sum">ITEMS 3</div>
-              <div className="col text-right">&euro; 132.00</div>
-            </div>
-            {/* <form>
-              <p>SHIPPING</p>
-              <select>
-                <option className="text-muted">
-                  Standard-Delivery- &euro;5.00
-                </option>
-              </select>
-              <p>GIVE CODE</p>
-              <input id="code" placeholder="Enter your code" />
-            </form> */}
-            <div className="row order__subtotal">
-              <div className="col">TOTAL PRICE</div>
-              <div className="col text-right">&euro; 137.00</div>
-            </div>
-            <button className="btn">CHECKOUT</button>
-          </div>
+          <OrderList
+            counts={counts}
+            setCounts={setCounts}
+            products={products}
+          />
+          <Summary sumUp={sumUp} total={total()} />
         </div>
       </div>
     </>
