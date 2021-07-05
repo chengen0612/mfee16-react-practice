ad56bbfb0aedd4ee99aefa8a686e0d14217a0871
chengen0612 <chengen0612@gmail.com>
2021-07-05 21:54:21 +0800

    update: complete edit feature but don't know reason

--- react-0628/.tmp/todo/components/TodoApp.js
+++ react-0628/.tmp/todo/components/TodoApp.js
@@ -1,18 +1,18 @@
 import { useState } from 'react'
 
 export default function ToDoApp() {
-  // todo = {id: number, text:string, completed: bool}
+  // todo = { id: number, text:string, completed: bool, edited: false }
   const [todos, setTodos] = useState([
     { id: 1, text: 'Hello', completed: false, edited: false },
     { id: 2, text: 'World', completed: false, edited: false },
   ])
 
-  const [todoInput, setTodoInput] = useState('')
+  const [text, setText] = useState('')
 
   const handleAddTodo = (e) => {
     if (e.key === 'Enter') {
       const newTodo = {
-        id: +new Date(),
+        id: +new Date(), // microsecond
         text: e.target.value,
         completed: false,
         edited: false,
@@ -24,8 +24,8 @@ export default function ToDoApp() {
 
   const handleCompleted = (id) => {
     const newTodos = [...todos]
-    // find index of item whose event target
-    // 因為索引值可能因資料變動而改變，所以要用比對 id 的方式去找目標
+    // compare id of todos to event target
+    // in some cases index might be different after data change
     const index = newTodos.findIndex((symbol, i) => {
       return symbol.id === id
     })
@@ -43,37 +43,29 @@ export default function ToDoApp() {
     setTodos(newTodos)
   }
 
-  const handleEdited = (id, e) => {
-    console.log('outside')
-
+  const handleEdited = (id) => {
     const newTodos = [...todos]
-    // find index of item whose event target
-    // 因為索引值可能因資料變動而改變，所以要用比對 id 的方式去找目標
+    // compare id of todos to event target
     const index = newTodos.findIndex((symbol, i) => {
       return symbol.id === id
     })
 
     if (index > -1) {
-      // setText(e.target.value)
-      // console.log(text)
       newTodos[index].edited = !newTodos[index].edited
-
-      const value = e.target.value
-      if (value !== 0) newTodos[index].text = value
-
+      newTodos[index].text = text
       setTodos(newTodos)
     }
   }
 
-  const editedTodoInput = (value) => {
+  const editedTodoInput = (text, setText) => {
     return (
       <>
         <input
           type="text"
-          value={value}
-          // onChange={(e) => {
-          //   setText(e.target.value)
-          // }}
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value)
+          }}
         />
       </>
     )
@@ -84,10 +76,6 @@ export default function ToDoApp() {
       <h1>代辦事項</h1>
       <input
         type="text"
-        value={todoInput}
-        onChange={(e) => {
-          setTodoInput(e.target.value)
-        }}
         onKeyPress={(e) => {
           handleAddTodo(e)
         }}
@@ -104,12 +92,14 @@ export default function ToDoApp() {
                 }}
               />
               <span
-                onDoubleClick={(e) => {
-                  handleEdited(item.id, e)
+                onDoubleClick={() => {
+                  // console.log(item.text)
+                  setText(item.text)
+                  handleEdited(item.id)
                 }}
               >
                 {item.edited ? (
-                  editedTodoInput(item.value)
+                  editedTodoInput(text, setText)
                 ) : item.completed ? (
                   <del>{item.text}</del>
                 ) : (
